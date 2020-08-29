7bec9514d2f35cc47980a1df02baba5e7e67be92
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-08-29 15:18:02 +0530

    setting up npm start

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+//requiring global files
 const express = require('express')
 
+//requiring local files
+
+//defining functions
 const app = express()
 
+//By default at production level the port is 80 but I have defined port to be 8000
 const port = 8000
 
+
+//Checking if their is an error in running 
 app.listen(port,function(err){
     if(err){
-        console.log(`Error in running at ${port}`)
+        console.log(`Error in running at port: ${port}`)
     }
-    console.log(` running at ${port}`)
+    console.log(` running at  port: ${port}`)
 
 })
