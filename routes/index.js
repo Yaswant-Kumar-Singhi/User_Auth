4371e16976e3b29725c065dbeb90663e7db0c4e6
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-08-29 15:35:43 +0530

    setting up home controller

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const router = express.Router();
 
+const home_controller = require('../controller/home_controller')
+
+router.get('/',home_controller.home)
+
 console.log('router is runing')
 
 module.exports = router
