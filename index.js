770262651cfeb90adb27fbce85e03ef3830821b3
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-08-30 19:43:36 +0530

    adding scss

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //requiring global files
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const sassMiddleware = require('node-sass-middleware')
 
 //requiring local files
 
@@ -13,6 +14,15 @@ app.use(express.static('./assets'));
 
 //By default at production level the port is 80 but I have defined port to be 8000
 const port = 8000
+
+//using saas middleware to convert scss files to css files
+app.use(sassMiddleware({
+    src : './assets/scss',
+    dest : './assets/css',
+    debug : true,
+    outputStyle : 'extended',
+    prefix : '/css'
+}))
 
 //we are defining it above routes because routes will be rendering some views and those
 //views belong from some sort of layouts.
