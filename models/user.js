bdd31e16a38c7af31a7a0aafb48f5d9966810540
Yaswant-Kumar-Singhi <singhiyaswant@gmail.com>
2020-09-04 16:59:59 +0530

    creating user schema

---
+++ models/user.js
@@ -0,0 +1,30 @@
+//requiring mongoose
+const mongoose = require('mongoose');
+
+
+//creatig user schema
+const userSchema = new mongoose.Schema({
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    securityPin : {
+        type : String,
+        required : true
+    },
+    name : {
+        type : String,
+        required :true
+    }
+},{
+    timestamps : true
+});
+
+//creating user model and exporting it
+const user = mongoose.model('user',userSchema);
+module.exports = user;
